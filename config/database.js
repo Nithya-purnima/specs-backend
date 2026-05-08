0151f8c745356b556e6e29c874d0b2b47be3df4b
Nithya-purnima <nithyapurnima05@gmail.com>
2026-05-08 20:44:30 +0530

    connected neon postgres

--- config/database.js
+++ config/database.js
@@ -2,26 +2,18 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false,
-
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
     }
-);
+});
 
-// Test database connection
 const testConnection = async () => {
     try {
         await sequelize.authenticate();
